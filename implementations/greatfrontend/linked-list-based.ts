e36cb449781c9707c8fc298ee251b3267e84f855
stormymayday <stormymayday7@gmail.com>
2025-01-06 17:05:50 -0800

    changed 'next' to 'prev'

--- implementations/greatfrontend/linked-list-based.ts
+++ implementations/greatfrontend/linked-list-based.ts
@@ -1,9 +1,9 @@
 class Node<T> {
     value: T;
-    next: Node<T> | null;
+    prev: Node<T> | null;
     constructor(value: T) {
         this.value = value;
-        this.next = null;
+        this.prev = null;
     }
 }
 
