a314d19ff5f58c88fbff7a0a635ccc454a0c1fbf
stormymayday <stormymayday7@gmail.com>
2025-01-06 17:32:40 -0800

    push

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import Stack from "@/implementations/greatfrontend/array-based";
+
+const myStack = new Stack();
+// console.log(myStack);
+console.log(myStack.push(0));
+
 export default function Home() {
     return (
         <div>
--- implementations/greatfrontend/array-based.ts
+++ implementations/greatfrontend/array-based.ts
@@ -3,4 +3,13 @@ export default class Stack<T> {
     constructor() {
         this._items = [];
     }
+
+    /**
+     * Pushes an item onto the top of the stack.
+     * @param {*} item The item to be pushed onto the stack.
+     * @return {number} The new length of the stack.
+     */
+    push(item: T): number {
+        return this._items.push(item);
+    }
 }
